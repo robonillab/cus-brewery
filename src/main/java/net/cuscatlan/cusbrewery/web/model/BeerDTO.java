ae5f3bbe6864fe97435dc6828a36ee2fdb851b11
rbonilla <renato_bonilla@dai.com>
2022-08-16 15:38:44 -0600

    Commit del DTO, probando el por qué los borra

---
+++ src/main/java/net/cuscatlan/cusbrewery/web/model/BeerDTO.java
@@ -0,0 +1,25 @@
+package net.cuscatlan.cusbrewery.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+/*
+Creado por Renato Bonilla 16/8/2022 : 15:32
+*/
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BeerDTO {
+
+    private UUID beerId;
+    private String beerName;
+    private String beerType;
+    private Long upc;
+
+}
