6bf5b0baa4adf151c904cc6b80e8624460e95b25
rbonilla <renato_bonilla@dai.com>
2022-08-16 14:47:44 -0600

    Primer Commit

---
+++ src/main/java/net/cuscatlan/cusbrewery/CusBreweryApplication.java
@@ -0,0 +1,13 @@
+package net.cuscatlan.cusbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CusBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CusBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/cuscatlan/cusbrewery/CusBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cuscatlan.cusbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CusBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
